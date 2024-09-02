e5f81ff32e6a352a76402773357ffd4720442a61
TheKeons <katie.emblow@gmail.com>
2024-09-02 19:39:30 +0200

    ferdig gjort oblig2

--- inf-1100/C/oblig2/oppgave5.c
+++ inf-1100/C/oblig2/oppgave5.c
@@ -15,10 +15,9 @@ int main(){
         printf("Hva ønsker du å konverterer til? 1 for Center, 2 for Newton og 3 for Karat\n");
         scanf("%d", &meny);
 
-        if (meny < 3 && meny > 1){
+        if (meny <= 3 && meny >= 1){
             if (meny == 1){
                 new_value = kilo * 0.01;
-                printf("%f %f", new_value, kilo);
                 måleenhet = "Center";
 
             }
@@ -35,6 +34,9 @@ int main(){
             printf("%.1f kilo tilsvarer %f %s\n", kilo, new_value, måleenhet);
             printf("Ønsker du å kjøre en gang til? 1 for ja alle andre tegn for nei\n");
             scanf("%d", &loop);
+        }
+        else {
+            printf("Velg tall fra 1 til 3\n");
         }
     }
 }
---
+++ inf-1100/C/oblig2/oppgave6.c
@@ -0,0 +1,34 @@
+#include <stdio.h>
+#include <string.h>
+
+int tversumm(char tall[], int len);
+
+int main(){
+    // siden strings er arrays av char velger jeg å skrive tallet som string sånn at jeg kan splitte detloope gjennom 
+    char tall[] = "1111"; 
+    int len = strlen(tall);
+    printf("Tversummen av %s er %d\n", tall, tversumm(tall, len));
+
+}
+
+int tversumm(char tall[], int len){
+    int i;
+    int tverrsummen = 0;
+    char ny_tall[10];
+    int ny_len;
+
+    for (i = 0; i < len; i++){
+        tverrsummen = tverrsummen + (tall[i] - '0'); // gjør hver char om til int og legger de sammen
+    }
+    sprintf(ny_tall, "%d", tverrsummen); // omgjør tversummen til string
+    ny_len = strlen(ny_tall);
+
+    // hvis tallet er større en 10 så kan jeg enda finne en tversum
+    // derfor kan jeg kaller jeg funksjonen på nytt
+    if (ny_len > 1){
+        tversumm(ny_tall, ny_len);
+    } 
+    else {
+        return tverrsummen;
+    }
+}
