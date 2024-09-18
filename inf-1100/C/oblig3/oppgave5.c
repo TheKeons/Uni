8fb4e11b1249f384cfbe96588c343af0bf2aae3c
Kathryn <katie.emblow@gmail.com>
2024-09-18 12:59:26 +0200

    modified:   inf-1100/C/oblig3/oppgave5.c

--- inf-1100/C/oblig3/oppgave5.c
+++ inf-1100/C/oblig3/oppgave5.c
@@ -8,3 +8,11 @@ int main () {
 
     return 0;
 }
+
+/*  Den orginale koden har mye unødvendig. For det første er det ingen grunn til å bruke dobbel pointer når man kun skal printe ut ting.
+    I dette tilfellet er det heller ingen vits å definere pointeren som en egen variabel, jeg valgte heller å definere stringen som en ponter,
+    slik som i linje 6, fordi jeg ikke ska endre stringen.
+    Det er heller ingen grunn til å prøve å endre på pekeren slik i det orginale programet når man kan finne det man leter ette ved bruk av indexer.
+    Når man skal finne det siste elementet av arrayen er det også viktig å ta lengden - 1 fordi indexer starter på 0.
+    Jeg valgte også å skrive lengden og print funksjonene på en linje bare fordi.
+*/
